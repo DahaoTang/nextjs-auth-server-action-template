38f178229e89c7a7c99b02db45131e92739bf55d
Dahao Tang <dahaotang@gmail.com>
2024-01-01 01:49:58 +1100

    landing page signin button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<div>
-			<div>Main Page</div>
-		</div>
+		<main className="h-full flex flex-col items-center justify-center">
+			<div className="space-y-6 text-center">
+				<div className="text-6xl">Auth</div>
+				<div>
+					<LoginButton>
+						<Button>Sign In</Button>
+					</LoginButton>
+				</div>
+			</div>
+		</main>
 	);
 }
