ab6af6d41b65f8c3b68ac3e0e9d7543bcd8f5794
Dahao Tang <dahaotang@gmail.com>
2024-01-01 02:42:52 +1100

    login page updated

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,16 +2,7 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 import { LoginForm } from "@/components/auth/login-form";
 
 const LoginPage = () => {
-	return (
-		<CardWrapper
-			headerLabel="Welcome back"
-			backButtonLabel="Don't have an account?"
-			backButtonHref="/auth/register"
-			showSocial
-		>
-			<LoginForm />
-		</CardWrapper>
-	);
+	return <LoginForm />;
 };
 
 export default LoginPage;
